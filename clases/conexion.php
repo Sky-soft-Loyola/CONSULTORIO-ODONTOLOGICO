56dbaf990d065e7a7bd07205f484a1945a4af396
CAMI1401 <147523867+CAMI1401@users.noreply.github.com>
2023-10-16 09:51:44 -0400

    CONEXION A BASE DE DATOS
    IMPLEMENTACION DE CODIGO PDO
    
    Co-Authored-By: Carolyn20 <138076418+Carolyn20@users.noreply.github.com>

--- clases/conexion.php
+++ clases/conexion.php
@@ -1,4 +1,23 @@
 <?php
 
+class Conexion{
+    private $host = "localhost";
+    private $user = "root";
+    private $password = "";
+    private $db = "clinica_odonto (1)";
+    private $conect;
 
+    public function__construct(){
+        $connectionString = "mysql:hos=".$this->host.";dbname=".$this->db.";charset=utf8";
+        try{
+            $this->conect = new PDO($connectionString,$this->user,$this->password);
+            $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión exitosa";
+        }   catch(Exception $e){
+            $this->conect="Error de conexión";
+            echo "ERROR: ".$e->getMessage();
+        }     
+    }
+}
+$conect = new conexion();
 ?>
