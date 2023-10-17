1c640ae779797891f5d999265ce7124025f077e9
ppuk-medina <Jose.Medina@fundacion-jala.org>
2023-10-16 21:24:19 -0400

    coneccion a la base de datos: registro de antecendentes medicos
    Se creo el metodo insertar_Antecedente  perteneciente al archivo conexion.php dentro del objeto de Conexion, ademas de que en la clase main se implemento el ejemplo de inserta un nuevo antecedente medico para usarlo mas frecuente
    
    Co-Authored-By: CAMI1401 <147523867+CAMI1401@users.noreply.github.com>
    Co-Authored-By: Carolyn20 <138076418+Carolyn20@users.noreply.github.com>

--- clases/conexion.php
+++ clases/conexion.php
@@ -1,39 +1,64 @@
 <?php
-
+require_once("paciente.php");
+require_once("antecedente.php");
 class Conexion{
     private $host = "localhost";
     private $user = "root";
     private $password = "";
     private $db = "clinica_odonto";
-    private $conect;
-
+    private $conexion;
+// str   --- class
     public function __construct(){
-        $connectionString = "mysql:hos=".$this->host.";dbname=".$this->db.";charset=utf8";
+        $connectionString = "mysql:host=".$this->host.";dbname=".$this->db.";charset=utf8";
         try{
-            $this->conect = new PDO($connectionString,$this->user,$this->password);
-            $this->conect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            echo "Conexión exitosa";
+            $this->conexion = new pdo($connectionString,$this->user,$this->password);
+            $this->conexion->setAttribute(pdo::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión exitosa <br>";
         }   catch(Exception $e){
-            $this->conect="Error de conexión";
+          //  $this->conexion="Error de conexión";
             echo "ERROR: ".$e->getMessage();
         }     
     }
+    
     public function insertar_Antecedente($paciente){
-        $sql="INSERT INTO 'antecedentes_medicos'('cod_antecedente','alergias','medicacion','patologia','observacion',tratamiento_medico') Values(?,?,?,?,?);";
-        $insert = $this->conect->prepare($sql);
-        $lista_datos = array($paciente->antecedentes->cod_antecedentes,
-                            $paciente->antecedentes->alergias,
-                            $paciente->antecedentes->medicacion,
-                            $paciente->antecedentes->patologia,
-                            $paciente->antecedentes->observacion,
-                            $paciente->antecedentes->tratamiento_medico,);
-        $resInsert = $insert->execute($lista_datos);
-        echo($resInsert);
+        $sql="INSERT INTO antecedentes_medicos (cod_antecedente, alergias, medicacion, patologia, observacion, tratamiento_medico) VALUES (?,?,?,?,?,?);";
+        $insert = $this->conexion->prepare($sql);    
+        $arrData = array($paciente->getAntecedentes()->getCod_antecedentes(),
+                         $paciente->getAntecedentes()->getAlergias(),
+                         $paciente->getAntecedentes()->getMedicacion(),
+                         $paciente->getAntecedentes()->getPatologia(),
+                         $paciente->getAntecedentes()->getObservacion(),
+                         $paciente->getAntecedentes()->getTratamiento_medico());     
+                         
+        try{
+            $insert->execute($arrData);
+        } catch (Exception $e){
+            echo("Ya existe este registro");
+        }         
     }
 
-  //  public function insertar_Usuario($paciente){
-  //  $sql=
-//}
+    public function insertar_Usuario($paciente){
+        //crear este metodo
+    }
+
+
+    public function getConexion(){
+        return $this->conexion;
+    }
+
+
 }
+
+
+
+//$antecendente=new Antecedentes_medicos(61004,"comidas","medicacion","patologia","observacion","tratamiento");
+//$paciente=new Paciente($antecendente,null,"jose","medina","avceran","2023/01/10","asasdas@asdad.ss","M","Bolivia","estudiante","solterio",124250296,73728149,"asdasdas");
+
 $conect = new conexion();
+//$conect->insertar_Antecedente($paciente);
+
+
+
+
+
 ?>
--- clases/main.php
+++ clases/main.php
@@ -5,6 +5,7 @@ require ("./paciente.php");
 require ("./odontologo.php");
 require ("./historial.php");
 require ("./antecedente.php");
+require_once("./conexion.php");
 //print_r ($_POST);
 
 //$persona = new Persona($_POST['ci'],$_POST['celular'],$_POST['direccion']);
@@ -14,7 +15,7 @@ require ("./antecedente.php");
 
 
 echo ("<hr>Esta es la clase acompañante<br>");
-$acompanante = new Acompañante($_POST['ci_acompanante'],$_POST['nombre_completo'],$_POST['celular_acompanante'],$_POST['parentesco'],$_POST['direccion_acompanante']);
+$acompanante = new Acompanante($_POST['ci_acompanante'],$_POST['nombre_completo'],$_POST['celular_acompanante'],$_POST['parentesco'],$_POST['direccion_acompanante']);
 
 $acompanante->mostrar_acompanante();
 
@@ -31,12 +32,13 @@ $antecedentes_medicos = new Antecedentes_medicos($_POST['cod_antecedentes'],$_PO
 
 $antecedentes_medicos->mostrar_antecedentes();
 
+
 echo ("<hr>Esta es la clase paciente<br>");
 $paciente = new Paciente($antecedentes_medicos,$acompanante,$_POST['nombre'],$_POST['ap_paterno'],$_POST['ap_materno'],$_POST['fecha_nacimiento'],$_POST['correo'],$_POST['genero'],$_POST['nacionalidad'],$_POST['ocupacion'],$_POST['estado_civil'],$_POST['ci_paciente'],$_POST['celular_paciente'],$_POST['direccion_paciente']);
 
 $paciente->mostrar_paciente();
 echo ("<hr>");
-
+$conect->insertar_Antecedente($paciente);
 echo ("<hr>Esta es la clase historial clinico<br>");
 $historial_clinico = new Historial_clinico($odontologo,$paciente,$_POST['cod_atencion'],$_POST['fecha'],$_POST['dientes'],$_POST['estado_diente'],$_POST['tratamiento'],$_POST['observacion']);
 
