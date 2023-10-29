4f90cb015ba729486925be2dd0c20b8287578714
ppuk-medina <Jose.Medina@fundacion-jala.org>
2023-10-29 13:53:49 -0400

    union de los archivos del anterior sprint

---
+++ prueba.php
@@ -0,0 +1,29 @@
+<?php
+
+if($_POST['enviar'])
+    echo "recibido con esxito";
+    echo "nombre = ",$_POST['nombre'],"<br>";
+    echo "apellido_paterno = ",$_POST['apellido_paterno'],"<br>";
+    echo "apellido_materno = ",$_POST['apellido_materno'],"<br>";
+    echo "ci = ",$_POST['ci_paciente'],"<br>";
+    echo "celular = ",$_POST['celular'],"<br>";
+    echo "ocupacion = ",$_POST['ocupacion'],"<br>";
+    echo "fecha_nacimiento = ",$_POST['fecha_nacimiento'],"<br>";
+    echo "correo = ",$_POST['correo'],"<br>";
+    echo "nacionalidad = ",$_POST['nacionalidad'],"<br>";
+    echo "estado_civil = ",$_POST['estado_civil'],"<br>";
+    echo "direccion = ",$_POST['direccion'],"<br>";
+    echo "genero = ",$_POST['genero'],"<br>";
+    echo "patologias = ",$_POST['patologias'],"<br>";
+    echo "alergia = ",$_POST['alergia'],"<br>";
+    echo "embarazo = ",$_POST['embarazo'],"<br>";
+    echo "tratamiento = ",$_POST['tratamiento'],"<br>";
+    echo "medicamento = ",$_POST['medicamento'],"<br>";
+    echo "obseracion = ",$_POST['obseracion'],"<br>";
+    echo "nombre_acompanante = ",$_POST['nombre_acompanante'],"<br>";
+    echo "celular_acompanante = ",$_POST['celular_acompanante'],"<br>";
+    echo "direccion_acompanante = ",$_POST['direccion_acompanante'],"<br>";
+    echo "parentes_acompanante = ",$_POST['parentes_acompanante'],"<br>";
+    echo "ci_acompanante = ",$_POST['ci_acompanante'],"<br>";
+
+?>
